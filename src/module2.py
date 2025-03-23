d6968a732f4636cbb832e93e279dcf69dc1dbf02
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2025-03-23 21:49:05 +0100

    added function that will return a DataFrame with the input as list of DividendEvents

--- src/module2.py
+++ src/module2.py
@@ -1,5 +1,5 @@
 import yfinance as yf
-
+import pandas as pd
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Optional, List
@@ -23,3 +23,15 @@ def get_dividend_events(ticker: str, start: datetime, end: datetime) -> List[Div
     return [
         DividendEvent(ex_date=date.to_pydatetime(), dividend=amount) for date, amount in filtered.items()
     ]
+
+def events_to_dataframe(events: List[DividendEvent]) -> pd.DataFrame:
+    data = [{
+        "ex_date": e.ex_date,
+        "dividend": e.dividend,
+        "buy_date": e.buy_date,
+        "sell_date": e.sell_date,
+        "price_buy": e.price_buy,
+        "price_sell": e.price_sell,
+        "total_return": e.total_return
+    } for e in events]
+    return pd.DataFrame(data)
