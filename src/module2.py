fccddac0c41777059accc5f935689cb7753c84d1
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2025-03-23 21:47:14 +0100

    added function that will return a list of DividendEvents

--- src/module2.py
+++ src/module2.py
@@ -1,3 +1,4 @@
+import yfinance as yf
 
 from dataclasses import dataclass
 from datetime import datetime
@@ -12,3 +13,13 @@ class DividendEvent:
     price_buy: Optional[float] = None
     price_sell: Optional[float] = None
     total_return: Optional[float] = None
+
+def get_dividend_events(ticker: str, start: datetime, end: datetime) -> List[DividendEvent]:
+    stock = yf.Ticker(ticker)
+    dividends = stock.dividends
+    dividends.index = dividends.index.tz_localize(None)
+    filtered = dividends[(dividends.index >= start) & (dividends.index <= end)]
+
+    return [
+        DividendEvent(ex_date=date.to_pydatetime(), dividend=amount) for date, amount in filtered.items()
+    ]
