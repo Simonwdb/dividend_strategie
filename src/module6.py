f3be52fd7aedd3337da22d23d6ce7524f713c4be
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2025-03-23 23:59:17 +0100

    added graph that plots the distribution of the total return over de ex-dividend dates

--- src/module6.py
+++ src/module6.py
@@ -5,3 +5,14 @@ import pandas as pd
 def plot_returns_over_time(df: pd.DataFrame) -> px.line:
     fig = px.line(df, x='ex_date', y='total_return', title='Return over time', markers=True)
     return fig
+
+def plot_return_distribution(df: pd.DataFrame) -> px.bar:
+    fig = px.bar(
+        df.sort_values('ex_date'),
+        x='ex_date',
+        y='total_return',
+        color='total_return',
+        color_continuous_scale='RdYlGn',
+        title='Return per Event'
+    )
+    return fig
