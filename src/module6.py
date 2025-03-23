a5210e83e1f69d3bb334cef68dc84d334a1d29af
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2025-03-23 22:21:23 +0100

    added dashboard template with tabs and their corresponding headers

---
+++ src/module6.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+
+st.set_page_config(page_title='Dividend Strategy Dashboard', layout='wide')
+st.title('Dividend Capture Strategy Dashboard')
+
+# Tabs
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs([
+    'Strategy Setup',
+    'Results',
+    'Analyze Results'
+    'Visuals (individual)',
+    'Annual Analysis',
+    'Visuals (comparison)'
+])
+
+with tab1:
+    st.header('Strategy Setup')
+
+with tab2:
+    st.header('Dividend Events & Trades')
+
+with tab3:
+    st.header('Summarize and Reports')
+
+with tab4:
+    st.header('Visuals per Ticker')
+
+with tab5:
+    st.header('Annual Analysis')
+
+with tab6:
+    st.header('Comparison multiple Tickers')
